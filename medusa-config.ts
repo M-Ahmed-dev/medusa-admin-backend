6dba6a10c282844edac0c3b9c49226d279df9217
M-Ahmed-dev <winterfell551@gmail.com>
2025-06-20 18:21:22 +0500

    changes

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,25 +3,13 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
-  admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-  },
-  plugins: [
-    {
-      resolve: "@medusajs/admin",
-      /** @type {import('@medusajs/admin').PluginOptions} */
-      options: {
-        path: "app",
-      },
-    },
-  ],
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL || "postgres",
     redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS || "*",
-      adminCors: process.env.ADMIN_CORS || "*",
-      authCors: process.env.AUTH_CORS || "*",
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
