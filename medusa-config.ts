1ce33fdcea92d2ac1e69d4a1be3958aa7fb0fc6e
noman hameed <nomanbutt8322@gmail.com>
2025-06-25 13:41:39 +0500

    fixed

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,31 +1,26 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-
-
-// import { loadEnv } from "@medusajs/medusa";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
-// loadEnv(); // loads .env variables
 
-const config = {
+module.exports = defineConfig({
   projectConfig: {
-    database_url: process.env.DATABASE_URL,
-    jwtSecret: process.env.JWT_SECRET || "supersecret",
-    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    databaseUrl: process.env.DATABASE_URL || "postgres",
+    // redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS || "*",
-      adminCors: process.env.ADMIN_CORS || "*",
-      authCors: process.env.AUTH_CORS || "*",
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
   plugins: [
-    {
-      resolve: "@medusajs/admin-bundler",
-      options: {
-        outDir: "dist/admin",
-      },
+  {
+    resolve: "@medusajs/admin-bundler",
+    options: {
+      outDir: ".medusa/admin"
     },
-    // add other plugins if needed
-  ],
-};
+  },
+],
+});
 
-export default config;
